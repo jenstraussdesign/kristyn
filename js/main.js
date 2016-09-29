68f1cfe15d855d226fec1015325b13782e3c52e5
Woo Kim <wrk1991@gmail.com>
2016-09-28 23:42:38 -0400

    fixed JS / styling completed

--- js/main.js
+++ js/main.js
@@ -22,3 +22,25 @@ tabs.map(function(tab) {
     });
 });
 
+
+function setActive(event) {
+  if (event.target.tagName !== 'A') {
+    return false;
+  }
+  
+  addActiveClass(event.target);
+}
+
+function addActiveClass(link) {
+  var activeLink = document.getElementsByClassName('active');
+  if (activeLink.length) {
+    activeLink[0].className = '';
+  }
+  
+  link.className = 'active';
+}
+
+window.onload = function () {    
+  var nav = document.getElementsByClassName('nav');
+  nav[0].addEventListener('click', setActive);
+}
